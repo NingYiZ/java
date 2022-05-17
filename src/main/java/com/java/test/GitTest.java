d9d3fa11e5aa8ed01189583dc726731990b7de2a
zyn <zyn_stu@163.com>
2022-05-17 19:55:31 +0800

    first commit

---
+++ src/main/java/com/java/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.java.test;
+
+/**
+ * @auther zyn
+ * @creat 2022-05-17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
